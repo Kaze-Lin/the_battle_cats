210f1a5a4c584a699f892a2f6fdf2a637dcbb556
Build428 <synyupw@gmail.com>
2026-05-22 14:02:20 +0800

    renew_stages

--- src/AppEnd.cpp
+++ src/AppEnd.cpp
@@ -1,8 +1,8 @@
 #include "App.hpp"
-
+#include "UserManager.hpp"
 #include "Util/Logger.hpp"
-
 
 void App::End() { // NOLINT(this method will mutate members in the future)
     LOG_TRACE("End");
+    UserManager::GetInstance().SaveDatabase();
 }
